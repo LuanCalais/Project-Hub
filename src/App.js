371228ce0ea7008adfadcf606c7ee71b51334fa2
LuanCalais <luancalais@gmail.com>
2023-01-28 12:43:53 -0300

    criando componente form e mudando favicon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Form from "./components/Form";
 import Header from "./components/Header";
-
 
 function App() {
   return (
-   <Header />
+    <>
+      <Header />
+      <div className="content">
+        <Form />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Form/index.js
@@ -0,0 +1,11 @@
+import "./Form.css";
+
+const Form = () => {
+  return (
+    <div className="form">
+        <h2>Crie seu time</h2>
+    </div>
+  );
+};
+
+export default Form;
