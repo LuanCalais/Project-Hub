78697b225ade1e70449ae7f0b3ec78fe6bf254f1
LuanCalais <luancalais@gmail.com>
2023-02-20 13:48:05 -0300

    create footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,33 @@
+import "./Footer.css";
+import icon from "./Icon.png";
+
+const Footer = ({ today }) => {
+  return (
+    <>
+      <div className="footer">
+        <div className="box-info">
+          <div className="col-one">
+            <img src={icon} alt="icon" />
+            <span>2023, Luan S. Calais</span>
+          </div>
+
+          <div className="col-two">
+            <a href="https://www.instagram.com/luan_calais/" target="_blank" rel="noreferrer">
+              <i class="bx bxl-instagram-alt"></i>
+            </a>
+
+            <a href="https://api.whatsapp.com/send?phone=11910396496" target="_blank" rel="noreferrer">
+              <i class="bx bxl-whatsapp"></i>
+            </a>
+
+            <a href="https://www.linkedin.com/in/luan-s-calais-186104217/" target="_blank" rel="noreferrer">
+              <i class="bx bxl-linkedin-square"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
