13380258c7c3d3ab1b687de51f7f2a6f5e5df67d
LuanCalais <luancalais@gmail.com>
2023-03-06 20:48:08 -0300

    install uuid lib

--- src/components/RootPage/index.js
+++ src/components/RootPage/index.js
@@ -3,35 +3,42 @@ import Footer from "../Footer";
 import Form from "../Form";
 import Header from "../Header";
 import Team from "../Team";
+import { v5 as id } from "uuid";
 import { useState } from "react";
 
 const RootPage = () => {
   const teams = [
     {
+      id: id(),
       label: "Develop",
       value: "Develop",
       primaryColor: "#d9f7e9",
       secondaryColor: "#57c278",
     },
     {
+      id: id(),
       label: "Designer",
       value: "Designer",
       primaryColor: "#b2cffa",
       secondaryColor: "#e8f8ff",
     },
     {
+      id: id(),
       label: "Scrum Master",
       value: "Scrum Master",
       primaryColor: "#a6d157",
       secondaryColor: "#f0f8e2",
     },
     {
+      id: id(),
       label: "Quality Analyst",
       value: "Quality Analyst",
       primaryColor: "#ffba05",
       secondaryColor: "#fff5d9",
     },
   ];
+
+
   const today = useState("");
 
   const [collaborators, setCollaborators] = useState([]);
@@ -53,18 +60,17 @@ const RootPage = () => {
       </div>
 
       <div className="box-team">
-        {
-            teams.map(team => (
-                <Team 
-                    key={team.label}
-                    label={team.label}
-                    primaryColor={team.primaryColor}
-                    secondaryColor={team.secondaryColor}
-                    collaborators={collaborators.filter(col => col.team === team.label)}
-                />
-
-            ))
-        }
+        {teams.map((team) => (
+          <Team
+            key={team.label}
+            label={team.label}
+            primaryColor={team.primaryColor}
+            secondaryColor={team.secondaryColor}
+            collaborators={collaborators.filter(
+              (col) => col.team === team.label
+            )}
+          />
+        ))}
       </div>
 
       <Footer today={today} />
