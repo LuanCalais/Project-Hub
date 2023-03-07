c52d7a91a6d1dcd231d71bb5b145e665936fa10a
LuanCalais <luancalais@gmail.com>
2023-03-06 21:05:33 -0300

    initiate delete collaborator from team implement

--- src/components/RootPage/index.js
+++ src/components/RootPage/index.js
@@ -3,34 +3,34 @@ import Footer from "../Footer";
 import Form from "../Form";
 import Header from "../Header";
 import Team from "../Team";
-import { v5 as id } from "uuid";
+import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
 
 const RootPage = () => {
   const teams = [
     {
-      id: id(),
+      id: uuidv4(),
       label: "Develop",
       value: "Develop",
       primaryColor: "#d9f7e9",
       secondaryColor: "#57c278",
     },
     {
-      id: id(),
+      id: uuidv4(),
       label: "Designer",
       value: "Designer",
       primaryColor: "#b2cffa",
       secondaryColor: "#e8f8ff",
     },
     {
-      id: id(),
+      id: uuidv4(),
       label: "Scrum Master",
       value: "Scrum Master",
       primaryColor: "#a6d157",
       secondaryColor: "#f0f8e2",
     },
     {
-      id: id(),
+      id: uuidv4(),
       label: "Quality Analyst",
       value: "Quality Analyst",
       primaryColor: "#ffba05",
@@ -38,14 +38,17 @@ const RootPage = () => {
     },
   ];
 
-
   const today = useState("");
-
   const [collaborators, setCollaborators] = useState([]);
 
   const addNewCollaborator = (col) => {
+    col.id = uuidv4();
     setCollaborators([...collaborators, col]);
   };
+
+  function deleteCollaborator(id) {
+    setCollaborators(collaborators.filter((col) => col.id !== id));
+  }
 
   return (
     <>
@@ -69,6 +72,7 @@ const RootPage = () => {
             collaborators={collaborators.filter(
               (col) => col.team === team.label
             )}
+            deleteCollaborator={deleteCollaborator}
           />
         ))}
       </div>
